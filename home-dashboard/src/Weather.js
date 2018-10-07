73e91bcc6e44bc03a7efae698632df10ae10fdc5
Jake Schoolmeesters <jake.schoolmeesters@icfolson.com>
2018-10-07 16:19:14 -0500

    restructured HTML and started the Weather tile component

---
+++ home-dashboard/src/Weather.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class WeatherTile extends Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Hello, weather will go here</h2>
+            </div>
+        );
+    }
+}
+
+
+export default WeatherTile;
--- home-dashboard/src/index.js
+++ home-dashboard/src/index.js
@@ -2,14 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import ToDoList from './todolist';
+import WeatherTile from "./Weather";
 
 var destination = document.querySelector("#container");
 
 ReactDOM.render(
-    <div>
-        <h2>Home page of Home Base Application</h2>
-        <div className="todo__main">
+    <div className="homeapp">
+        <div className="homeapp-title">
+            <h2>Home page of Home Base Application</h2>
+        </div>
+
+        <div className="homeapp-todo__main">
             <ToDoList />
+        </div>
+        
+        <div className="homeapp-weather__main">
+            <WeatherTile />
         </div>
     </div>,
     destination

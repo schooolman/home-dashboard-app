2449f567dde3b3988c408a2fed6a693bce94ab0f
Jake Schoolmeesters <jake.schoolmeesters@icfolson.com>
2018-10-07 17:56:48 -0500

    hooking up the component to the weather API.

--- home-dashboard/src/Weather.js
+++ home-dashboard/src/Weather.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 
 class WeatherTile extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null,
+        }
+    };
+
+    componentDidMount() {
+        fetch('http://api.openweathermap.org/data/2.5/weather?zip=60640,us&APPID=b22b825c1197355edeef5ad8a62cc1a6')
+        .then(response => {
+            response.json();
+            console.log(response);
+        })
+        .then(
+            data => this.setState({ data })
+        )
+        ;
+        console.log( this.state );
+
+
+    }
+
     render() {
         return (
             <div>

06cd9d281f8e52478a553420b0aa306eec885fa4
Jake Schoolmeesters <jake.schoolmeesters@icfolson.com>
2018-10-14 20:24:17 -0500

    finally have the weather displaying from the API onto the main page of the app.

--- home-dashboard/src/Weather.js
+++ home-dashboard/src/Weather.js
@@ -7,15 +7,19 @@ class WeatherTile extends React.Component {
         const data = this.props.weather;
         // let goodData = Object.keys(data);
 
-        // console.log(goodData);
+        let tempFarenheit = Math.round((data.temp - 273.15) * 9 / 5 + 32);
 
         console.log(data);
-        console.log((typeof this.props.weather));
+        // console.log((typeof this.props.weather));
 
         return (
             <div>
                 <h2>Hello, weather will go here</h2>
                 {/* <span>{name}</span> */}
+                {/* { data } */}
+                <h3> City: Chicago (Andersonville)</h3>
+                <h3> Temp: {tempFarenheit}</h3>
+                <h3> Sky: {data.sky}</h3>
             </div>
 
         

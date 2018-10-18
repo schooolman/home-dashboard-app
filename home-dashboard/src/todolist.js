b28a71a9cb49e9204b18b1fabbffa78beeb27077
Jake Schoolmeesters <jake.schoolmeesters@icfolson.com>
2018-10-17 21:25:56 -0500

    initializing and starting firebase setup.

--- home-dashboard/src/todolist.js
+++ home-dashboard/src/todolist.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TodoItems from "./TodoItems";
+import firebase from './firebase.js';
 import "./todolist.css";
 
 class ToDoList extends Component {
@@ -7,34 +8,43 @@ class ToDoList extends Component {
         super(props);
 
         this.state = {
-            items: []
+            items: [],
+            currentItem: ''
         };
 
         this.addItem = this.addItem.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     addItem(e) {
-
         if (this._inputElement.value !== "") {
             var newItem = {
                 text: this._inputElement.value,
                 key: Date.now()
             };
-
             this.setState((prevState) => {
                 return {
                     items: prevState.items.concat(newItem)
                 };
             });
-
             this._inputElement.value = "";
         }
-
         console.log(this.state.items);
-
         e.preventDefault();
+    }
 
+    handleSubmit(e) {
+        e.preventDefault();
+        const itemsRef = firebase.database().ref('items');
+        const item = {
+            title: this.state.currentItem,
+            // user: this.state.username
+        }
+        itemsRef.push(item);
+        this.setState({
+            currentItem: '',
+        })
     }
 
     deleteItem(key) {
@@ -51,7 +61,8 @@ class ToDoList extends Component {
         return (
             <div className="todoListMain">
                 <div className="header">
-                    <form onSubmit={this.addItem}>
+                    <form onSubmit={this.handleSubmit}>
+                    {/* <form onSubmit={this.addItem}> */}
                         <input
                             ref={(a) => this._inputElement = a} 
                             placeholder="Enter a task"
